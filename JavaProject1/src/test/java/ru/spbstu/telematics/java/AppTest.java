495ed99f488baa997826a3fee20329b7459d52dd
Vladislav Gaar <gaarvlad@gmail.com>
2024-10-29 17:51:45 +0300

    Implemented tests for App's class multiplyMatrices method

--- JavaProject1/src/test/java/ru/spbstu/telematics/java/AppTest.java
+++ JavaProject1/src/test/java/ru/spbstu/telematics/java/AppTest.java
@@ -3,6 +3,7 @@ package ru.spbstu.telematics.java;
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
+import java.util.Arrays;
 
 /**
  * Unit test for simple App.
@@ -25,14 +26,87 @@ public class AppTest
      */
     public static Test suite()
     {
-        return new TestSuite( AppTest.class );
+        return new TestSuite(AppTest.class);
     }
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
+
+    public void testMultiplyMatrices()
     {
-        assertTrue( true );
+    	int[][] matrix1 = {
+    		{1,2,3},
+    		{4,5,6},
+    		{7,8,9}
+    	};
+
+    	int[][] matrix2 = {
+    		{3,3,3},
+    		{5,1,3},
+    		{2,1,2}
+    	};
+
+    	int[][] expected = {
+			{19,8,15},
+			{49,23,39},
+			{79,38,63}
+    	};
+
+    	int [][] result = App.multiplyMatrices(matrix1, matrix2);
+
+    	for (int i = 0; i < expected.length; i++) {
+    		assertTrue(Arrays.equals(expected[i], result[i]));
+    	}
     }
+
+    
+    public void testIdentityMatrix()
+    {
+    	int[][] matrix = {
+    		{1,2,3},
+    		{4,5,6},
+    		{7,8,9}
+    	};
+
+    	int[][] identity = {
+    		{1,0,0},
+    		{0,1,0},
+    		{0,0,1}
+    	};
+
+   
+    	int [][] result = App.multiplyMatrices(matrix, identity);
+
+    	for (int i = 0; i < matrix.length; i++) {
+    		assertTrue(Arrays.equals(matrix[i], result[i]));
+    	}
+    }
+
+    
+    public void testZeroMatrix()
+    {
+    	int[][] matrix = {
+    		{1,2,3},
+    		{4,5,6},
+    		{7,8,9}
+    	};
+
+    	int[][] identity = {
+    		{0,0,0},
+    		{0,0,0},
+    		{0,0,0}
+    	};
+
+
+    	int [][] result = App.multiplyMatrices(matrix, identity);
+
+    	int[][] expected = {
+    		{0,0,0},
+    		{0,0,0},
+    		{0,0,0}
+    	};
+
+    	for (int i = 0; i < expected.length; i++) {
+    		assertTrue(Arrays.equals(expected[i], result[i]));
+    	}
+    }
+    
 }
