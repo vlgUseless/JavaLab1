6c1665885be6e214080a26ec0e1c93f5b1a30f2a
Vladislav Gaar <gaarvlad@gmail.com>
2024-10-29 17:32:35 +0300

    Implemented multiplyMatrices method in App class

--- JavaProject1/src/main/java/ru/spbstu/telematics/java/App.java
+++ JavaProject1/src/main/java/ru/spbstu/telematics/java/App.java
@@ -1,13 +1,23 @@
 package ru.spbstu.telematics.java;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main(String[] args)
+    {}
+
+    public static int[][] multiplyMatrices(int[][] matrix1, int[][] matrix2)
     {
-        System.out.println( "Hello World!" );
-    }
+	    final int length = matrix1.length;
+		int[][] matrixResult = new int[length][length];
+		
+	    for (int row = 0; row < length; row++) {
+	       	for (int column = 0; column < length; column++) {
+	       		for (int elem = 0; elem < length; elem++) {
+	       			matrixResult[row][column] += matrix1[row][elem] * matrix2[elem][column];      			        			        		        	         
+        		}
+			}
+	    }
+
+	    return matrixResult;
+	}
 }
