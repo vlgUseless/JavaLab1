a391d1bc36b0bd53076bfe6a2050c21f247fdbe4
Vladislav Gaar <gaarvlad@gmail.com>
2024-12-24 23:40:38 +0300

    Added test in main method

--- JavaProject1/src/main/java/ru/spbstu/telematics/java/App.java
+++ JavaProject1/src/main/java/ru/spbstu/telematics/java/App.java
@@ -3,7 +3,25 @@ package ru.spbstu.telematics.java;
 public class App 
 {
     public static void main(String[] args)
-    {}
+    {
+        int[][] matrix1 = {
+                {1, 2, 3},
+                {4, 5, 6}
+        };
+
+        int[][] matrix2 = {
+                {7, 8},
+                {9, 10},
+                {11, 12}
+        };
+
+        try {
+            int[][] result = multiplyMatrices(matrix1, matrix2);
+            printMatrix(result);
+        } catch (IllegalArgumentException e) {
+            System.err.println("Ошибка: " + e.getMessage());
+        }
+    }
 
     /**
      * Метод для перемножения двух матриц.
